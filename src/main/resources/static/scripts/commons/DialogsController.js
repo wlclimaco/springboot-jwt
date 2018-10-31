cad3e2c15d3f79670cf5f04c3cae5a5fe441147f
Washington <Washington@DESKTOP-K7J0AHG>
2018-10-31 01:19:50 -0200

    sadsdasdasd

--- src/main/resources/static/scripts/commons/DialogsController.js
+++ src/main/resources/static/scripts/commons/DialogsController.js
@@ -27,10 +27,10 @@
     };
 
   $scope.openSolicitacao = function (size, parentSelector,jogo,status) {
-	  debugger
+	  
 		var abUl = $location.absUrl(); 
-	//	  var surl = "https://quadra-test.herokuapp.com/#/pages/signin";
-		  var surl = "http://localhost:8080/#/pages/signin";
+		  var surl = "https://quadra-test.herokuapp.com/#/pages/signin";
+	//	  var surl = "http://localhost:8080/#/pages/signin";
 		  if(abUl === surl){
 			  $scope.remover = false;
 			  $scope.desmarcar = false;
